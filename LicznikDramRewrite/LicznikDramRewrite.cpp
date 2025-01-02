085ebf6ee2ceafb64578262dc91f19fe965de85a
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2025-01-02 10:13:14 +0100

    edit

--- LicznikDramRewrite/LicznikDramRewrite.cpp
+++ LicznikDramRewrite/LicznikDramRewrite.cpp
@@ -1,9 +1,16 @@
 ﻿#include <iostream>
 #include <cstdio>
+#include <fstream>
+#include <string>
 using namespace std;
 int main() {
 
 	string input;
+	string read;
+	string textread;
+	ifstream text("c:\\Users\\Public\\Documents\\dramas.txt");
+	getline(text,textread);
+	cout << "Dram juz bylo: " << textread;
 
 	cout << "LicznikDramRewrite, Nowy LicznikDram z wiki o dramach  Y - Byla drama, N - nie bylo dramy wiki - Wiki dram info - info o programie";
 	cin >> input;
@@ -11,6 +18,9 @@ int main() {
 
 	if (input == "Y") {
 		cout << "o Drama";
+		ofstream licznik("c:\\Users\\Public\\Documents\\dramas.txt");
+		licznik << "drama";
+		licznik.close();
 	}
 
 
